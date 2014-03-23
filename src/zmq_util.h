55a5776d283d228464133743f24ded290f0dd44f
Joe McIlvain <joe.eli.mac@gmail.com>
2014-03-23 16:54:33 -0700

    Move logic to convert QByteArray to and from encoded data strings to ZMQ_Util static methods

--- src/zmq_util.h
+++ src/zmq_util.h
@@ -11,11 +11,17 @@ class ZMQ_Util : public QObject
     
 public:
     static QObject* qmlAttachedProperties(QObject* object);
+    
+    static inline const char* dataChars()
+    { return " !\"#$&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"; };
+    
+    static inline QString convertBytesToData(const QByteArray& bytes)
+    { return QString::fromLatin1(bytes.toPercentEncoding(dataChars())); }
+    
+    static inline QByteArray convertDataToBytes(const QString& str)
+    { return QByteArray::fromPercentEncoding(str.toLatin1()); }
 };
 
-
-#define LATIN1_CHARS \
-" !\"#$&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
 
 class ZMQ_UtilAttached : public QObject
 {
@@ -30,10 +36,10 @@ private:
     
 public slots:
     QString convertLatin1ToData(const QString& str)
-    { return QString::fromLatin1(str.toLatin1().toPercentEncoding(LATIN1_CHARS)); }
+    { return ZMQ_Util::convertBytesToData(str.toLatin1()); }
     
-    QString convertDataToLatin1(const QString& str)
-    { return QString::fromLatin1(QByteArray::fromPercentEncoding(str.toLatin1())); }
+    QString convertDataToLatin1(const QString& data)
+    { return QString::fromLatin1(ZMQ_Util::convertDataToBytes(data)); }
 };
 
 
